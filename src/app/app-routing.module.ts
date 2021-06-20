be0541ce5aa09e572ab20c24c2865eaba7709248
swaroopamir <mailtoswaroop24@gmail.com>
2021-06-20 18:04:36 +0530

    Initial Commit 1. Angular Project Creation 2. Integrating Bootstrap and Angular Material 3. Created Product,Stocks and Supplier components 4. Navigating b/w components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { SuppliersComponent } from "./components/suppliers/suppliers.component";
+import { StocksComponent } from "./components/stocks/stocks.component";
+import { ProductsComponent } from "./components/products/products.component";
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "Products", component: ProductsComponent },
+  { path: "Stocks", component: StocksComponent },
+  { path: "Suppliers", component: SuppliersComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ProductsComponent } from "./components/products/products.component";
+import { StocksComponent } from "./components/stocks/stocks.component";
+import { SuppliersComponent } from "./components/suppliers/suppliers.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatTableModule } from "@angular/material/table";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    StocksComponent,
+    SuppliersComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTableModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
